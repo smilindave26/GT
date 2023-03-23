6e91f8f6cd8e7c53c7c1c4f7c9b4dcf6b9c99ee0
Dave Hart <dphart@rochester.rr.com>
2023-03-23 14:17:12 -0400

    Initial Commit

---
+++ GT/GTApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTApp.swift
+//  GT
+//
+//  Created by David Hart on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct GTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
